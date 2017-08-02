3a5ade5ed89d96e9fe9f57c2a0d60d6421252e2a
Daniel Cunha <danielbrandaodacunha@gmail.com>
2017-08-02 16:14:48 -0300

    Done

---
+++ mirage/config.js
@@ -0,0 +1,98 @@
+export default function () {
+  this.namespace = '/api';
+
+  let rentals = [{
+    type: 'rentals',
+    id: 'grand-old-mansion',
+    attributes: {
+      title: 'Grand Old Mansion',
+      owner: 'Veruca Salt',
+      city: 'San Francisco',
+      "property-type": 'Estate',
+      bedrooms: 15,
+      image: 'https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg',
+      description: "This grand old mansion sits on over 100 acres of rolling hills and dense redwood forests."
+    }
+  }, {
+    type: 'rentals',
+    id: 'urban-living',
+    attributes: {
+      title: 'Urban Living',
+      owner: 'Mike Teavee',
+      city: 'Seattle',
+      "property-type": 'Condo',
+      bedrooms: 1,
+      image: 'https://upload.wikimedia.org/wikipedia/commons/0/0e/Alfonso_13_Highrise_Tegucigalpa.jpg',
+      description: "A commuters dream. This rental is within walking distance of 2 bus stops and the Metro."
+    }
+  }, {
+    type: 'rentals',
+    id: 'downtown-charm',
+    attributes: {
+      title: 'Downtown Charm',
+      owner: 'Violet Beauregarde',
+      city: 'Portland',
+      "property-type": 'Apartment',
+      bedrooms: 3,
+      image: 'https://upload.wikimedia.org/wikipedia/commons/f/f7/Wheeldon_Apartment_Building_-_Portland_Oregon.jpg',
+      description: "Convenience is at your doorstep with this charming downtown rental. Great restaurants and active night life are within a few feet."
+    }
+  }];
+
+  this.get('/rentals', (db, request) => {
+    if(request.queryParams.city !== undefined){
+      let filteredResults = rentals.filter((i) => {
+        return i.attributes.city.toLocaleLowerCase().indexOf(request.queryParams.city.toLocaleLowerCase()) !== -1;
+      });
+      return {data: filteredResults};
+    } else {
+      return {data: rentals};
+    }
+  });
+
+  this.get('/rentals/:id', (db, request) =>{
+    return {data: rentals.find((rental) => request.params.id === rental.id)}
+  });
+
+
+
+   // this.get('/rentals', function () {
+    
+    // return {
+    //   data: [{
+    //     type: 'rentals',
+    //     id: 'grand-old-mansion',
+    //     attributes: {
+    //       title: 'Grand Old Mansion',
+    //       owner: 'Veruca Salt',
+    //       city: 'San Francisco',
+    //       "property-type": 'Estate',
+    //       bedrooms: 15,
+    //       image: 'https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg'
+    //     }
+    //   }, {
+    //     type: 'rentals',
+    //     id: 'urban-living',
+    //     attributes: {
+    //       title: 'Urban Living',
+    //       owner: 'Mike Teavee',
+    //       city: 'Seattle',
+    //       "property-type": 'Condo',
+    //       bedrooms: 1,
+    //       image: 'https://upload.wikimedia.org/wikipedia/commons/0/0e/Alfonso_13_Highrise_Tegucigalpa.jpg'
+    //     }
+    //   }, {
+    //     type: 'rentals',
+    //     id: 'downtown-charm',
+    //     attributes: {
+    //       title: 'Downtown Charm',
+    //       owner: 'Violet Beauregarde',
+    //       city: 'Portland',
+    //       "property-type": 'Apartment',
+    //       bedrooms: 3,
+    //       image: 'https://upload.wikimedia.org/wikipedia/commons/f/f7/Wheeldon_Apartment_Building_-_Portland_Oregon.jpg'
+    //     }
+    //   }]
+    // };
+  
+}
